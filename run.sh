369ec75aa519395aa8e2d2ecb6f4dfb9e29fdea7
poyuan <poyuan@tw.ibm.com>
2019-05-07 23:21:14 +0800

    process

---
+++ run.sh
@@ -0,0 +1,3 @@
+python3 main.py --source_domain mnistm --target_domain mnistm --training_mode target \
+--max_epoch 10 
+
--- util/preprocess.py
+++ util/preprocess.py
@@ -1,10 +1,15 @@
 import os, shutil
 
-data_dir = './data/MNIST_M'
-train_labels = './data/MNIST_M/mnist_m_train_labels.txt'
-test_labels = './data/MNIST_M/mnist_m_test_labels.txt'
-train_images = './data/MNIST_M/mnist_m_train'
-test_images = './data/MNIST_M/mnist_m_test'
+# dataset = 'mnistm'
+dataset = 'svhn'
+# dataset = 'usps'
+
+
+data_dir = '../hw3-sposusu/hw3_data/digits/'+ dataset
+train_labels = '../hw3-sposusu/hw3_data/digits/'+ dataset + '/train.csv'
+test_labels = '../hw3-sposusu/hw3_data/digits/'+ dataset + '/test.csv'
+train_images = '../hw3-sposusu/hw3_data/digits/'+ dataset + '/train'
+test_images = '../hw3-sposusu/hw3_data/digits/'+ dataset + '/test'
 
 def mkdirs(path):
     train_dir = path + '/' + 'train'
@@ -21,13 +26,16 @@ def mkdirs(path):
 
 def process(labels_path, images_path, data_dir):
     with open(labels_path) as f:
-        for line in f.readlines():
-            img = images_path + '/' + line.split()[0]
-            dir = data_dir + '/' + line.split()[1]
-            shutil.move(img, dir)
+        lines = iter(f.readlines())
+        next(lines)
+        for line in lines:
+            line = line.replace('\n','')
+            img = images_path + '/' + line.split(',')[0]
+            dir = data_dir + '/' + line.split(',')[1] + '/' + line.split(',')[0]
+            shutil.copyfile(img, dir)
 
 mkdirs(data_dir)
 process(train_labels, train_images, data_dir + '/train')
 process(test_labels, test_images, data_dir + '/test')
-os.remove(train_images)
-os.remove(test_images)
+# os.remove(train_images)
+# os.remove(test_images)
