cc1b663601236bf99efd4a6c41f21eff71134578
poyuan <poyuan@tw.ibm.com>
2019-05-08 16:16:05 +0800

    process

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python3 main.py --source_domain usps --target_domain mnistm --training_mode target \
+python3 main.py --source_domain mnistm --target_domain svhn --training_mode target \
 --max_epoch 10 
 
