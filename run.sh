788f6f993b7ea18280cadcd0c001180ba1601d38
poyuan <poyuan@tw.ibm.com>
2019-05-12 16:56:10 +0800

    zz

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python3 main.py --source_domain usps --target_domain mnistm --training_mode source --max_epoch 11 --fig_mode save
-python3 main.py --source_domain mnistm --target_domain svhn --training_mode source --max_epoch 11 --fig_mode save
-python3 main.py --source_domain svhn --target_domain usps --training_mode source --max_epoch 11 --fig_mode save
+python3 main.py --source_domain usps --target_domain mnistm --training_mode target --max_epoch 5 --fig_mode save | tee -a log-file
+python3 main.py --source_domain mnistm --target_domain svhn --training_mode targer --max_epoch 3 --fig_mode save | tee -a log-file
+python3 main.py --source_domain svhn --target_domain usps --training_mode source --max_epoch 3 --fig_mode save | tee -a log-file
 
